fd650b2051b7abc21c65885db48b81fc97398822
Alexey Vorobiev <alexey@itima.ru>
2020-04-15 21:56:46 +0300

    fix

--- processes/trade_placement_process.py
+++ processes/trade_placement_process.py
@@ -1,5 +1,5 @@
 from models.realtime import Realtime
 
 class TradePlacementProcess:
-    def run(self, pairs):
-        Realtime().run(pairs[0], pairs[1])
+    def run(self, pair1, pair2):
+        Realtime().run(pair1, pair2)
--- run_realtime_trading_process.py
+++ run_realtime_trading_process.py
@@ -1,4 +1,4 @@
 from processes.trade_placement_process import TradePlacementProcess
 import sys
 
-TradePlacementProcess().run(sys.argv[0], sys.argv[1])
+TradePlacementProcess().run(sys.argv[1], sys.argv[2])
